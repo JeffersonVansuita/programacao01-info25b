b02575e01756049c6ab9dd2891c96a89d6a19b4f
aluno <aluno@lab01-03>
2026-03-09 08:56:37 -0300

    primeiro bagulho

---
+++ a/src/main/java/com/mycompany/a/A.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.a;
+
+import java.util.Date;
+/**
+ *
+ * @author aluno
+ */
+public class A {
+
+    public class Estudante {
+        private String nome;
+        private date DataNascimento;
+        private String cpf;
+        private char genero;
+        private String matricula;
+        private String email;
+        
+        public Estudante(){
+            nome = "nobody";
+        }
+        public int ObterIdade(Date hoje){
+            int idade = 0;
+            return idade;
+        }
+        public String getNome(){
+        return nome;
+        }
+        public void setNome(String_nome){
+            nome = _nome;
+        }
+    }
+}
